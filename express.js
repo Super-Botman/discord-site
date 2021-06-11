05792148085de15a66742370d16c1b3f4cf120dc
Botman <velayismaa@gmail.com>
2021-06-11 18:56:49 +0200

    finnal project

--- express.js
+++ express.js
@@ -1,54 +1,52 @@
 /*******************************************/
-/*     this app are created by Botman      */ 
+/*     this app are created by Botman      */
 /* discord : My name is Super Botman#6621  */
-/* github : Super-Botman                   */
+/*            github : Super-Botman                   */
 /*******************************************/
 
 const express = require('express');
 const Discord = require('discord.js');
 const fs = require('fs');
 const mongoose = require('mongoose');
-const mongodb = require('mongodb');
+const generator = require('./generator.js');
+const messageModel = require('./message.js');
 
 /**************************/
-/* Variables for the code */ 
+/* Variables for the code */
 /**************************/
 
-const Schema = mongoose.Schema;
 const app = express();
 const client = new Discord.Client();
-const messageSchema = new Schema({
-  content: String,
-  author: String,
-  id_msg: Number
-});
-const messageModel = mongoose.model('message', messageSchema)
 const MongoClient = require('mongodb').MongoClient;
-const { rootCertificates } = require('tls');
+const {
+  rootCertificates
+} = require('tls');
 const url = 'mongodb://root:root@localhost:27017';
 const dbName = 'messages';
-const options = { useUnifiedTopology: true }
+const options = {
+  useUnifiedTopology: true
+}
 let db
 
 /************************/
-/* Connection to the db */ 
+/* Connection to the db */
 /************************/
 
-MongoClient.connect(url ,options,function (err, client) {
+MongoClient.connect(url, options, function (err, client) {
   console.log("Connected successfully to server");
   db = client.db(dbName);
 });
 
-/****************************/
-/* start of the discord bot */ 
-/****************************/
+/********************************************/
+/* message for the start of the discord bot */
+/********************************************/
 
 client.on('ready', () => {
-  console.log(`${client.user.tag} est lancer il est partit écumer les océans de discord!`);
+  console.log(`${client.user.tag} is ready`);
 });
 
 /*********************/
-/* message collector */ 
+/* message collector */
 /*********************/
 
 client.on('message', message => {
@@ -66,35 +64,77 @@ client.on('message', message => {
   mongoose.set('useNewUrlParser', true);
   mongoose.connect("mongodb://mongoadmin:mongoadmin@127.0.0.1:27017/messages?authSource=admin")
     .then(() => {
-      console.log("connected to DB.")
       messageData.save().then(result => {
-        console.log('Message ajouté avec succès !');
         mongoose.connection.close()
       })
     })
     .catch(err => console.log(err));
+  generator.generateHtml()
 })
 
 /*********************/
-/* website launcher */ 
+/* website launcher */
 /********************/
 
 app.get('/', function (req, res) {
-  db.collection('messages').find({}).toArray(function (err, docs) {
-    if (err) {
-      console.log(err)
-      throw err
-    }
-    res.status(200).json(docs)
+  let response 
+  fs.readFile("./webpage/index.html", "utf8", function (err, donnees) {
+    response = donnees
+    if (err) throw err;
+    res.status(200)
+    res.type('html')
+    res.end(response)
   })
 });
+
+app.get('/message.html', function (req, res) {
+  let response 
+  fs.readFile("./webpage/message.html", "utf8", function (err, donnees) {
+    response = donnees
+    if (err) throw err;
+    res.status(200)
+    res.type('html')
+    res.end(response)
+  })
+});
+
+app.get('/messages.js', function (req, res) {
+  fs.readFile("./webpage/messages.js", "utf8", function (err, donnees) {
+    response = donnees
+    if (err) throw err;
+    res.status(200)
+    res.type('script')
+    res.end(response)
+  })
+})
+
+app.get('/style.css', function (req, res) {
+  fs.readFile("./webpage/style.css", "utf8", function (err, donnees) {
+    response = donnees
+    if (err) throw err;
+    res.status(200)
+    res.type('text/css')
+    res.end(response)
+  })
+})
+
+app.get('/index.css', function (req, res) {
+  fs.readFile("./webpage/index.css", "utf8", function (err, donnees) {
+    response = donnees
+    if (err) throw err;
+    res.status(200)
+    res.type('text/css')
+    res.end(response)
+  })
+})
 
 app.listen(3000, function () {
   console.log('app listening on port 3000!');
 });
 
 /*********************/
-/* login of the bot  */ 
+/* login of the bot  */
 /*********************/
 
-client.login('ODM5OTE4NjMyNDE4NjcyNjUw.YJQpLg.VTAdPVQ3tt8oV3B4vMwW1ud8v0A')
+
+client.login('ODM5OTE4NjMyNDE4NjcyNjUw.YJQpLg.Bp6KXEFBdnc75pekXfoTrksCthU')
